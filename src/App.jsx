3e56726e551688b309fdaa61dc33cf16db802750
Matheus <matheusrobertjs@gmail.com>
2024-11-16 08:50:08 -0300

    config theme tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { Button } from '@material-tailwind/react';
 function App() {
-	return (
-		<>
-			<Button>Button</Button>
-			<p className="text-3xl">Click on the Vite and React logos to learn more</p>
-		</>
-	);
+	return <></>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ const withMT = require('@material-tailwind/react/utils/withMT');
 module.exports = withMT({
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'blue-dark': '#1E6F9F',
+				blue: '#4EA8DE',
+				'purple-dark': '#5E60CE',
+				purple: '#8284FA',
+				gray: {
+					700: '#0D0D0D',
+					600: '#1A1A1A',
+					500: '#262626',
+					400: '#333333',
+					300: '#808080',
+				},
+				danger: '#E25858',
+			},
+			fontFamily: {
+				sans: ['DM Sans', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 });
