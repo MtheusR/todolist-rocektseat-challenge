7b58eb14462a2001c26dc157520458f934fbe997
Matheus <matheusrobertjs@gmail.com>
2024-11-16 09:22:21 -0300

    feat: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import { Button } from '@material-tailwind/react';
-function App() {
-	return <></>;
-}
+import { SectionInput } from './SectionInput';
+export function App() {
+	return (
+		<>
+			<main className="bg-gray-600 h-screen w-s">
+				<header className="flex justify-center bg-gray-700 p-20">
+					<img src="/logotipo.svg" alt="logo todo" />
+				</header>
 
-export default App;
+				<SectionInput />
+			</main>
+		</>
+	);
+}
---
+++ src/SectionInput.jsx
@@ -0,0 +1,5 @@
+import { Input } from '@material-tailwind/react';
+
+export function SectionInput() {
+	return <></>;
+}
